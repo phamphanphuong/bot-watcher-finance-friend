13c8d0320d30edb45699a3f426684525e4ad3e25
phamphanphuong <23210266@ms.uit.edu.vn>
2025-04-12 23:23:54 +0700

    'ok'

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/bot-watcher-finance-friend/", // ✅ Quan trọng
   plugins: [
     react(),
     mode === 'development' &&
